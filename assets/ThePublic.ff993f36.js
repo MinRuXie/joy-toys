db0484569ce6df1433c4801c619bae9c70ff6d39
joyxie <joyxie@labspace.com.tw>
2025-01-16 15:45:45 +0800

    update

--- assets/ThePublic.ff993f36.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as t,c,b as s}from"./index.ee5e90be.js";const n={name:"ThePublic",setup(){}},a={class:"wrapper-inner public"};function p(_,i,u,l,m,d){const e=r("router-view");return t(),c("div",a,[s(e)])}var b=o(n,[["render",p]]);export{b as default};
